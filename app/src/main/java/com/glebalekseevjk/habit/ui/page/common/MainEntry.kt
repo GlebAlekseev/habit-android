1228bff45e33145ae2401773afd661ddbb80b670
GlebAlekseev <glebalekseevjk@gmail.com>
2023-04-17 16:53:01 +0300

    added splash & bottom bar

--- app/src/main/java/com/glebalekseevjk/habit/ui/page/common/MainEntry.kt
+++ app/src/main/java/com/glebalekseevjk/habit/ui/page/common/MainEntry.kt
@@ -1,23 +1,11 @@
 package com.glebalekseevjk.habit.ui.page.common
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import com.glebalekseevjk.habit.ui.page.splash.SplashPage
 import com.glebalekseevjk.habit.ui.theme.AppTheme
 
 @Composable
 fun MainEntry() {
-    var isSplash by rememberSaveable { mutableStateOf(true) }
     AppTheme {
-        if (isSplash) {
-            SplashPage {
-                isSplash = false
-            }
-        } else {
-            AppScaffold()
-        }
+        AppScaffold()
     }
 }
