53b547bf808de96c4d3f5a16ec557f6d9d6abe9e
GlebAlekseev <glebalekseevjk@gmail.com>
2023-04-26 22:01:53 +0300

    added edit habits modal bottom sheets

---
+++ app/src/main/java/com/glebalekseevjk/habit/ui/widgets/modal_bottom_sheet/ChooseTimeModalBottomSheet.kt
@@ -0,0 +1,80 @@
+package com.glebalekseevjk.habit.ui.widgets.modal_bottom_sheet
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.commandiron.wheel_picker_compose.WheelTimePicker
+import com.glebalekseevjk.habit.ui.theme.AppTheme
+import com.glebalekseevjk.habit.ui.theme.LargePadding
+import com.glebalekseevjk.habit.ui.theme.MiddlePadding
+import com.glebalekseevjk.habit.ui.theme.typography
+
+
+@Composable
+fun ChooseTimeModalBottomSheet() {
+    Column(
+        modifier = Modifier
+            .padding(MiddlePadding),
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = "Выберите время",
+            color = AppTheme.colors.colorOnPrimary,
+            style = typography.titleMedium,
+        )
+
+
+        WheelTimePicker(
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) { snappedTime ->
+            println(snappedTime)
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Button(
+                onClick = { },
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier,
+                contentPadding = PaddingValues(
+                    vertical = MiddlePadding,
+                    horizontal = LargePadding
+                ),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AppTheme.colors.colorOnPrimary,
+                    contentColor = AppTheme.colors.colorPrimary
+                )
+            ) {
+                Text(
+                    text = "Отмена".toUpperCase(),
+                    style = typography.titleMedium,
+                    color = AppTheme.colors.colorPrimary
+                )
+            }
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier,
+                contentPadding = PaddingValues(
+                    vertical = MiddlePadding,
+                    horizontal = LargePadding
+                ),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AppTheme.colors.colorOnPrimary,
+                    contentColor = AppTheme.colors.colorPrimary
+                )
+            ) {
+                Text(
+                    text = "Сохранить".toUpperCase(),
+                    style = typography.titleMedium,
+                    color = AppTheme.colors.colorPrimary
+                )
+            }
+        }
+    }
+}
