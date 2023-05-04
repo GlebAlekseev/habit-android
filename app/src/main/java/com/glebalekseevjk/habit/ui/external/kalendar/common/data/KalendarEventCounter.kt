32ef5fc0c2e0ddf7ba3f2e06e50f9d088e4e5ed7
GlebAlekseev <glebalekseevjk@gmail.com>
2023-05-04 18:48:23 +0300

    added calendar for history page

---
+++ app/src/main/java/com/glebalekseevjk/habit/ui/external/kalendar/common/data/KalendarEventCounter.kt
@@ -0,0 +1,8 @@
+package com.glebalekseevjk.habit.ui.external.kalendar.common.data
+
+import java.time.LocalDate
+
+data class KalendarEventCounter(
+    val date: LocalDate,
+    val counter: Int
+)
